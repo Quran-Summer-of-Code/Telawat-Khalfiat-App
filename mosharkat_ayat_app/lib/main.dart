ed6b4583ceccb24a23a63eafb008786e9541c05f
Ahmed Yasser Hussein <93765400+AhmedYasser20@users.noreply.github.com>
2024-10-11 14:06:47 +0300

    Refactor app title and description

--- mosharkat_ayat_app/lib/main.dart
+++ mosharkat_ayat_app/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         initialRoute: RouteClass.home,
         onGenerateRoute: RouteClass.generateRoute,
-        title: 'Mosharkat Ayat App',
+        title: 'تلاوات_مع_خلفيات',
       ),
     );
   }
